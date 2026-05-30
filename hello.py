c50040ca464552a204839f07e1e29bd47858f9a6
Tim Mitra <tim@it-guy.com>
2026-05-30 12:16:58 -0400

    first python

---
+++ hello.py
@@ -0,0 +1,67 @@
+# Output Hello World! to the screen
+'''
+This is a comment and it's long
+So I'm going to use two lines for it
+'''
+
+import os
+os.system("clear") # clears the terminal screen
+
+print("Hello World!") # I learned about comments!
+print(2 + 3)
+print(5 - 1)
+print(3 * 4)
+print(10 / 5)
+print(35 % 3) # prints remainder of the operation, eg. 2
+print(2 ** 4) # prints 4 to the power of 2
+
+print(f"3 + 2 = {3 + 2}")
+print(f"5 - 1 = {5 - 1}")
+print(f"3 * 4 = {3 * 4}")
+print(f"10 / 5 = {10 / 5}")
+print(f"35 % 3 = {35 % 3}")
+print(f"2 ** 4 = {2 ** 4}")
+
+print(5 > 1)
+
+print(f"5 == 1: {5 == 1}")
+print(f"5 != 1: {5 != 1}")
+print(f"5 > 1: {5 > 1}")
+print(f"5 >= 1: {5 >= 1}")
+print(f"5 < 1: {5 < 1}")
+print(f"5 <= 1: {5 <= 1}")
+
+my_name = "Tim Mitra"
+print(my_name)
+
+number_1 = 5
+number_2 = 10
+print(number_1 + number_2)
+
+fruit_1 = "apples"
+fruit_2 = "oranges"
+print("I Like " + fruit_1 + " and I like " + fruit_2)
+
+fruit_1 = "apples"
+print(fruit_1 * 5)
+
+number_1 = 14
+number_1 = number_1 + 27
+print(number_1)
+
+# user input
+
+#####################
+# Data Types
+# String
+# Integer
+# Float
+# Boolean
+# List
+# Tuple
+# Dictionary
+#####################
+
+names = ["Tim", "Mitra", "John", "Doe"] # this is a list, it can be changed
+print(names[0]) # prints the first item in the list, which is "Tim"
+
